edc6d02263f328feb5b7d7c875bb65130c61a13f
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-03 19:13:20 +0530

    Adding more files

---
+++ 3_ReadCmd.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Please Enter your Username"
+
+read USERNAME # this is the variable which will store the data you'll enter
+
+echo "Username you entered : $USERNAME"
+
+echo "Please Enter your Password"
+
+read PASSWORD # this is the variable which will store the password you'll enter
+
+echo "Username you entered : $PASSWORD"
