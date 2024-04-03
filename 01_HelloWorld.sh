f41a4832ef258cd05a25fe561d26b049084471fb
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-03 17:12:26 +0530

    added conversations

--- 01_HelloWorld.sh
+++ 01_HelloWorld.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
 echo "Hello , I'm learning devops with aws"
+
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Good Morning, Ramesh"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I'm Good, How are you Ramesh"
