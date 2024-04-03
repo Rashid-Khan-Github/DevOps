2363df674cbace27e2c7d0315194ea2605aef8c3
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-03 23:03:15 +0530

    Adding more files

--- 6_Install_MySql.sh
+++ 6_Install_MySql.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 VALIDATE(){
-    if ($1 -ne 0)
+    if [ $1 -ne 0 ]
     then
         echo "Installation Failed...!"
         exit 1
@@ -14,7 +14,7 @@ VALIDATE(){
 
 USERID=$(id -u)
 
-if ($USERID -ne 0)
+if [ $USERID -ne 0 ]
 then 
     echo "ERROR : Please run this script with a root permission"
     exit 1
