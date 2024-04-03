042a32feaa2c70d442092afc8d99c8c117c8345e
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-03 17:55:36 +0530

    added conversations

--- 01_HelloWorld.sh
+++ 01_HelloWorld.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 echo "Hello , I'm learning devops with aws"
 
-echo "Ramesh: Hi Suresh, Good Morning"
-echo "Suresh: Good Morning, Ramesh"
-echo "Ramesh: How are you Suresh"
-echo "Suresh: I'm Good, How are you Ramesh"
-echo "Ramesh: Are you learning UI Technologies, Suresh"
-echo "Suresh: Yes Ramesh, But it is taking more time"
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning, $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I'm Good, How are you $PERSON1"
+echo "$PERSON1: Are you learning UI Technologies, $PERSON2"
+echo "$PERSON2: Yes $PERSON1, But it is taking more time"
