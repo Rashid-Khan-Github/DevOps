03391518cd3c8c036482eed366a14883b2959d69
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-03 19:30:24 +0530

    Adding more files

---
+++ 4_Arrays.sh
@@ -0,0 +1,8 @@
+echo "Arrays can be used in the Shell Script as "
+
+PERSONS=("Ramesh", "Suresh", "Mahesh")
+
+echo "First Person : ${PERSONS[0]}"
+echo "Second Person: ${PERSONS[1]}"
+echo "Second Person: ${PERSONS[2]}"
+echo "All Persons: ${PERSONS[@]}"
