4da7f0560fe54c80ca1591826ba95234cd92d353
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-07 00:46:33 +0530

    Adding more files

--- 10_InsPackageLoop.sh
+++ 10_InsPackageLoop.sh
@@ -7,7 +7,7 @@ N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
-    echo " $R Error :: Please run this script with root access $N"
+    echo -e " $R Error :: Please run this script with root access $N"
     exit 1
 fi
 
