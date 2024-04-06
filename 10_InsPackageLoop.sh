6db52ee2c58f39132494e5aa4ed037d350c04125
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-07 00:42:19 +0530

    Adding more files

---
+++ 10_InsPackageLoop.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+USERID=$(id -u)
+
+R="\e[32m"
+N="\e[0m"
+
+if [$USERID -ne 0]
+then
+    echo " $R Error :: Please run this script with root access $N"
+    exit 1
+fi
+
+
+for i in $@
+do
+    yum install $i -y
+done
--- 9_loops.sh
+++ 9_loops.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+DATE=$(date +F%---%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME--$DATE.log
+
 for i in $@
 do
     yum install $i -y
