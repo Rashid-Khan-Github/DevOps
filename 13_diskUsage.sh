657c374cbb832cf810b08fa6a4e02411d15fdf1c
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-12 18:29:02 +0530

    Adding more files

--- 13_diskUsage.sh
+++ 13_diskUsage.sh
@@ -9,17 +9,35 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+# This command will extract only the line except tmpfs or Filesystem i.e /dev/xvdf portions only.
 DISK_USAGE=$(df -h | grep -vE "tmpfs|Filesystem")
-DISK_USAGE_THRESHOLD=1
+DISK_USAGE_THRESHOLD=2
 
 # Create and attach a new volume to you EC2 Running Instance...!
 # Volume must be available in the same AZ where your EC2 is running.
 
+# IFS= is the default Internal Field separator environment variable.
+# Default IFS value is space.
+# It will trim the extra whitespaces (no need but good practice to write)
+
+#Looping through all the available disk lines
+
+MESSAGE=""
 while IFS= read line
 do 
+    # this will extract the disk usage in number format
+    USAGE=$(echo $line | awk '{print $6}' | cut -d % -f 1)
 
-    echo "Output : $line"
+    # this command will extact the partition from the line
+    PARTITION=$(echo $line | awk '{print $1}')
+
+    #check whether disk usage is greater than threshold value
+    if [ $USAGE -gt $DISK_USAGE_THRESHOLD]
+    then
+        MESSAGE+="$R High Usage on disk $PARTITION : $USAGE $N \n"
+    fi
 
 done <<< $DISK_USAGE
 
-
+echo -e " $R MESSAGE ALERT : $MESSAGE $N"
+echo "$MESSAGE" | mail -s "Warning ! High Disk Usage Alert..." rashidkhan7805@gmail.com 
