ec59abbc81afccb826d6bfe39d969d785aa822f6
Rashid-Khan <rashidkhan7805@gmail.com>
2024-04-13 00:51:38 +0530

    Adding more files

--- mail.sh
+++ mail.sh
@@ -8,6 +8,6 @@ ALERT_TYPE=$5
 
 # echo "All Args : $@"
 
-FINAL_MSG=$(sed -e "s/TEAM_NAME/DevOps Team/g" -e "s/ALERT_TYPE/MEMORY ALERT !!/g" -e "s/MESSAGE/$BODY/" template.html)
+FINAL_MSG=$(sed -e "s/TEAM_NAME/DevOps Team/g" -e "s/ALERT_TYPE/MEMORY ALERT..!!/g" -e "s/MESSAGE/$BODY/" template.html)
 
 echo "$FINAL_MSG" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" $TO_ADDRESS
